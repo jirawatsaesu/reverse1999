f11981cc0290cc41f5d5d7c15f68552ee9791776
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2023-11-04 23:41:28 +0700

    [feature] solved puzzle v2

--- block_puzzle_solver.py
+++ block_puzzle_solver.py
@@ -2,50 +2,50 @@ import numpy as np
 
 # Define the puzzle size and the blocks
 puzzle_size = 6
-
 blocks = {
-    'long_z': np.array([
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1]
-    ]),
-    'l': np.array([
-        [1, 0],
-        [1, 0],
-        [1, 1]
-    ]),
-    'l2': np.array([
-        [1, 0],
-        [1, 0],
-        [1, 1]
-    ]),
-    'l3': np.array([
-        [1, 0],
-        [1, 0],
-        [1, 1]
-    ]),
-    'invert_l': np.array([
-        [0, 1],
-        [0, 1],
-        [1, 1]
-    ]),
-    'invert_l2': np.array([
-        [0, 1],
-        [0, 1],
-        [1, 1]
-    ]),
-    'invert_z': np.array([
-        [0, 1, 1],
-        [1, 1, 0]
-    ]),
-    'invert_z2': np.array([
-        [0, 1, 1],
-        [1, 1, 0]
-    ]),
-    'short_l': np.array([
-        [1, 0],
-        [1, 1]
-    ])
+    'long_z': {
+        'shape': np.array([
+            [1, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1]
+        ]),
+        'quantity': 1,
+        'id': 'Z'
+    },
+    'L': {
+        'shape': np.array([
+            [1, 0],
+            [1, 0],
+            [1, 1]
+        ]),
+        'quantity': 3,
+        'id': 'L'
+    },
+    'invert_l': {
+        'shape': np.array([
+            [0, 1],
+            [0, 1],
+            [1, 1]
+        ]),
+        'quantity': 2,
+        'id': '<'
+    },
+    'invert_z': {
+        'shape': np.array([
+            [0, 1, 1],
+            [1, 1, 0]
+        ]),
+        'quantity': 2,
+        'id': 'S'
+    },
+    'short_l': {
+        'shape': np.array([
+            [1, 0],
+            [1, 1]
+        ]),
+        'quantity': 1,
+        'id': 'sL'
+    }
 }
 
 block_ids = {
@@ -87,25 +87,34 @@ def remove_block(puzzle, block, pos):
                 puzzle[i + pos[0]][j + pos[1]] = ' '
     return puzzle
 
-def solve_puzzle(puzzle, blocks, idx=0):
-    if idx == len(blocks):
+def solve_puzzle(puzzle, blocks, block_keys=None, idx=0):
+    if block_keys is None:
+        block_keys = list(blocks.keys())
+    
+    if idx == len(block_keys):
         return True, puzzle
 
-    block_key = list(blocks.keys())[idx]
-    block_variants = [blocks[block_key]] + [rotate(blocks[block_key], i) for i in range(1, 4)]
-    block_id = block_ids[block_key]
+    block_key = block_keys[idx]
+    block_data = blocks[block_key]
+    block_variants = [block_data['shape']] + [rotate(block_data['shape'], i) for i in range(1, 4)]
+    block_id = block_data['id']
 
     for block in block_variants:
+        if block_data['quantity'] == 0:
+            break  # Skip if no more blocks of this type are left to place
         for i in range(puzzle_size - block.shape[0] + 1):
             for j in range(puzzle_size - block.shape[1] + 1):
                 if fits(puzzle, block, (i, j)):
                     puzzle = place_block(puzzle, block, (i, j), block_id)
-                    success, result = solve_puzzle(puzzle, blocks, idx + 1)
+                    blocks[block_key]['quantity'] -= 1  # Decrement the block quantity
+                    success, result = solve_puzzle(puzzle, blocks, block_keys, idx + (block_data['quantity'] == 0))
                     if success:
                         return True, result
                     puzzle = remove_block(puzzle, block, (i, j))
+                    blocks[block_key]['quantity'] += 1  # Increment the block quantity back
 
     return False, puzzle
+
 
 # Initialize the puzzle grid with spaces representing empty slots
 puzzle_grid = np.full((puzzle_size, puzzle_size), ' ', dtype='<U1')
