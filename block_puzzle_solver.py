b063252b25ce971e1f54b5e9ad8adfe22026315f
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2023-11-27 00:50:02 +0700

    [refactor] GUI

--- block_puzzle_solver.py
+++ block_puzzle_solver.py
@@ -22,9 +22,8 @@ class Application(tk.Frame):
         self.create_widgets()
         self.solution_labels = []  # List to keep track of solution labels
 
-    def create_widgets(self):
+    def create_puzzle_dimension_inputs(self):
         default_puzzle_size = 5
-
         self.width_label = tk.Label(self, text="Width:")
         self.width_label.grid(row=0, column=0)
         self.width_entry = tk.Entry(self)
@@ -37,57 +36,75 @@ class Application(tk.Frame):
         self.height_entry.insert(0, str(default_puzzle_size))
         self.height_entry.grid(row=1, column=1)
 
-        # Label for the block entries
+        self.height_entry.bind('<KeyRelease>', self.update_total_value)
+        self.width_entry.bind('<KeyRelease>', self.update_total_value)
+
+    def create_block_inputs(self):
         self.blocks_label = tk.Label(self, text="Blocks:")
         self.blocks_label.grid(row=2, column=0, columnspan=8)
 
-        # Dictionary to hold the quantity entries for each block
         self.block_entries = {}
-        i = 3  # Start at row 3 for the block entries
-        j = 0  # Start at column 0 for the block entries
+        i = 3  # Start at row 3 for block entries
+        j = 0  # Start at column 0 for block entries
+
         for block_name, block_info in blocks.items():
             block_visual = get_block_representation(block_info['shape'])
             block_label = tk.Label(self, text=block_visual, font=("Courier", 12), justify=tk.LEFT)
             block_label.grid(row=i, column=j)
 
             label = tk.Label(self, text=f"{block_name} quantity:")
-            label.grid(row=i, column=j+1)
-            
+            label.grid(row=i, column=j + 1)
+
             entry = tk.Entry(self)
-            entry.insert(0, "0")  # Set default value to 0
-            entry.grid(row=i+1, column=j+1)
-            
+            entry.insert(0, "0")  # Default quantity set to 0
+            entry.grid(row=i + 1, column=j + 1)
+            entry.bind('<KeyRelease>', self.update_total_value)
+
             self.block_entries[block_name] = entry
-            
-            # Increase column count by 2 for the next entry pair (label and entry)
-            j += 2
-            
-            # If we have added 4 input pairs, increase row count and reset column count
-            if j >= 8:
+
+            j += 2  # Increment column for next block
+            if j >= 8:  # If 4 blocks per row, move to next row
                 i += 2
                 j = 0
 
-        self.height_entry.bind('<KeyRelease>', self.update_total_value)
-        self.width_entry.bind('<KeyRelease>', self.update_total_value)
+    def create_solve_button(self):
+        self.total_volume = tk.Label(self, text="Total Block Value:")
+        self.total_volume.grid(row=100, column=0, columnspan=8)
 
-        for block_name, entry in self.block_entries.items():
-            entry.bind('<KeyRelease>', self.update_total_value)
-
-        # Volume display
-        self.total_volume = tk.Label(self, text="total")
-        self.total_volume.grid(row=i+2, column=0, columnspan=8)
-        self.total_volume["text"] = f"Total Block value: {self.total_value}"
-
-        # Solve button
         self.solve_button = tk.Button(self, text="Solve", command=self.solve)
-        self.solve_button.grid(row=i+3, column=0, columnspan=8)
+        self.solve_button.grid(row=101, column=0, columnspan=8)
 
-        # Result display
+    def create_result_display(self):
         self.result_label = tk.Label(self, text="")
-        self.result_label.grid(row=i+4, column=0, columnspan=8)
+        self.result_label.grid(row=200, column=0, columnspan=8)
+
+    def display_solution(self, solved_puzzle):
+        start_row = 200
+        for i, row in enumerate(solved_puzzle):
+            for j, cell in enumerate(row):
+                block_label = cell
+                color = block_colors.get(block_label, 'white')  # Fetch the color for the block
+
+                # Create a label for each cell in the solution grid
+                cell_label = tk.Label(self, text=f"{block_label}", bg=color, borderwidth=1, relief="solid")
+
+                # Adjust the row index by adding start_row to place it under the input fields
+                cell_label.grid(row=start_row + i, column=j, sticky="nsew", padx=1, pady=1)
+                self.solution_labels.append(cell_label)  # Add the label to the list
+
+        # Adjust row/column configurations for better display if needed
+        for i in range(len(solved_puzzle)):
+            self.grid_rowconfigure(start_row + i, weight=1)
+        for j in range(len(solved_puzzle[0])):
+            self.grid_columnconfigure(j, weight=1)
+
+    def create_widgets(self):
+        self.create_puzzle_dimension_inputs()
+        self.create_block_inputs()
+        self.create_solve_button()
+        self.create_result_display()
     
     def update_total_value(self, event=None):
-        # Reset the total value to 0 before calculating it again
         self.total_value = 0
 
         # Calculate the total value without solving the puzzle
@@ -148,29 +165,6 @@ class Application(tk.Frame):
                 self.result_label["text"] = "No solution exists."
         except ValueError:
             messagebox.showwarning("Invalid Input", "Please enter valid numbers for all fields.")
-
-    def display_solution(self, solved_puzzle):
-        start_row = 15  # Start after the buffer row and an additional space for clarity
-
-        # Display the solution grid
-        for i, row in enumerate(solved_puzzle):
-            for j, cell in enumerate(row):
-                block_label = cell
-                color = block_colors.get(block_label, 'white')  # Fetch the color for the block
-
-                # Create a label for each cell in the solution grid
-                cell_label = tk.Label(self, text=f"{block_label}", bg=color, borderwidth=1, relief="solid")
-
-                # Adjust the row index by adding start_row to place it under the input fields
-                cell_label.grid(row=start_row + i, column=j, sticky="nsew", padx=1, pady=1)
-                self.solution_labels.append(cell_label)  # Add the label to the list
-
-        # Adjust row/column configurations for better display if needed
-        for i in range(len(solved_puzzle)):
-            self.grid_rowconfigure(start_row + i, weight=1)
-        for j in range(len(solved_puzzle[0])):
-            self.grid_columnconfigure(j, weight=1)
-
 
 root = tk.Tk()
 root.title("Block Puzzle Solver")
